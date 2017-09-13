9e30be8a3f0c4a4c39845415da46ecd2e66408e9
kimmy241 <kimlouw.kl@gmail.com>
2017-09-13 17:25:17 +1200

    cases for operands

--- src/UnboundedInteger.java
+++ src/UnboundedInteger.java
@@ -9,23 +9,43 @@ public class UnboundedInteger {
     private LinkedList<Integer> data;
     public UnboundedInteger(String digits) {}
 
-    public UnboundedInteger add(UnboundedInteger num1, UnboundedInteger num2) {
+    public UnboundedInteger add(UnboundedInteger other) {
         UnboundedInteger result;
         return result;
     }
 
-    public UnboundedInteger substract(UnboundedInteger num1, UnboundedInteger num2) {
+    public UnboundedInteger substract(UnboundedInteger other) {
         UnboundedInteger result;
         return result;
     }
 
-    public UnboundedInteger multiply(UnboundedInteger num1, UnboundedInteger num2) {
+    public UnboundedInteger multiply(UnboundedInteger other) {
         UnboundedInteger result;
         return result;
     }
 
-    public UnboundedInteger divide(UnboundedInteger num1, UnboundedInteger num2) {
+    public UnboundedInteger divide(UnboundedInteger other) {
         UnboundedInteger result;
+        return result;
+    }
+
+    public UnboundedInteger gcd(UnboundedInteger other) {
+        UnboundedInteger result;
+        return result;
+    }
+
+    public boolean greaterThan(UnboundedInteger other) {
+        boolean result;
+        return result;
+    }
+
+    public boolean lessThan(UnboundedInteger other) {
+        boolean result;
+        return result;
+    }
+
+    public boolean equals(UnboundedInteger other) {
+        boolean result;
         return result;
     }
 
@@ -41,7 +61,7 @@ public class UnboundedInteger {
                 System.out.println(line);
                 continue;
             }
-            String[] numbers = line.split("\\2+");
+            String[] numbers = line.split("\\s+");
             if(numbers.length < 3) {
                 System.out.println("# Syntax error");
                 continue;
@@ -49,6 +69,38 @@ public class UnboundedInteger {
             String num1 = numbers[0];
             String operand = numbers[1];
             String num2 = numbers[2];
+            String result;
+            switch(operand) {
+                case "+":
+                    result = num1.add(num2); 
+                    break;
+                case "-":
+                    result = num1.subtract(num2);
+                    break;
+                case "*":
+                    result = num1.multiply(num2);
+                    break;
+                case "/":
+                    result = num1.divide(num2);
+                    break;
+                case "gcd":
+                    result = num1.gcd(num2);
+                    break;
+                case ">":
+                    result = num1.greaterThan(num2);
+                    break;
+                case "<":
+                    result = num1.lessThan(num2);
+                    break;
+                case "=":
+                    result = num1.equals(num2);
+                    break;
+                default:
+                    System.out.println("# Syntax error");
+            }
+            if(result != "") {
+                System.out.println("# " + result);
+            }
         }
         scanner.close();
      }
