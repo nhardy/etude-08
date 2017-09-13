08d43dc2be3235ff8dd35936e951158a1e6bec08
kimmy241 <kimlouw.kl@gmail.com>
2017-09-13 17:04:44 +1200

    reading in input

--- src/UnboundedInteger.java
+++ src/UnboundedInteger.java
@@ -9,6 +9,26 @@ public class UnboundedInteger {
     private LinkedList<Integer> data;
     public UnboundedInteger(String digits) {}
 
+    public UnboundedInteger add(UnboundedInteger num1, UnboundedInteger num2) {
+        UnboundedInteger result;
+        return result;
+    }
+
+    public UnboundedInteger substract(UnboundedInteger num1, UnboundedInteger num2) {
+        UnboundedInteger result;
+        return result;
+    }
+
+    public UnboundedInteger multiply(UnboundedInteger num1, UnboundedInteger num2) {
+        UnboundedInteger result;
+        return result;
+    }
+
+    public UnboundedInteger divide(UnboundedInteger num1, UnboundedInteger num2) {
+        UnboundedInteger result;
+        return result;
+    }
+
     public String toString() {
         return "";
     }
@@ -17,6 +37,18 @@ public class UnboundedInteger {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
+            if(line.charAt(0) == '#') {
+                System.out.println(line);
+                continue;
+            }
+            String[] numbers = line.split("\\2+");
+            if(numbers.length < 3) {
+                System.out.println("# Syntax error");
+                continue;
+            }
+            String num1 = numbers[0];
+            String operand = numbers[1];
+            String num2 = numbers[2];
         }
         scanner.close();
      }
