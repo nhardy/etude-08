594eacb070b9018e0d3aa9d3c924a88cca0da0d9
Nathan Hardy <nhardy@live.com.au>
2017-09-13 17:05:26 +1200

    Start

--- src/UnboundedInteger.java
+++ src/UnboundedInteger.java
@@ -1,13 +1,28 @@
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.regex.*;
 
 /**
  * UnboundedInteger class
  * @author Kimberley Louw, Nathan Hardy
  */
 public class UnboundedInteger {
-    private LinkedList<Integer> data;
-    public UnboundedInteger(String digits) {}
+    private boolean isNegative = false;
+    private LinkedList<Boolean> bits = new LinkedList<Boolean>();
+    private final Pattern stringPattern = Pattern.compile("^(-?)(\\d+)$");
+    public UnboundedInteger(String input) {
+        Matcher m = stringPattern.matcher(input);
+
+        if (!m.find()) throw new IllegalArgumentException("String: ''" + input + "'' does not match the pattern for UnboundedInteger.");
+
+        isNegative = m.group(1).length() == 1;
+        String digits = m.group(2);
+    }
+
+    public UnboundedInteger(boolean isNegative, LinkedList<Boolean> bits) {
+        this.isNegative = isNegative;
+        this.bits = bits;
+    }
 
     public String toString() {
         return "";
