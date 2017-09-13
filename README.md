dc0586ef0934c1c3759e03624b660245b41fb117
Nathan Hardy <nhardy@live.com.au>
2017-09-13 16:38:59 +1200

    Boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# etude-08
-COSC326 Étude 08
+# Étude 8
+
+Java solution for COSC326 Étude 8 - Arithmetic
+
+See http://www.cs.otago.ac.nz/cosc326/PDF/8-Arithmetic.pdf for more details
+
+## Usage
+
+```
+cd src
+javac UnboundedInteger.java
+java UnboundedInteger  < ../input.txt
+```
---
+++ src/UnboundedInteger.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * UnboundedInteger class
+ * @author Kimberley Louw, Nathan Hardy
+ */
+public class UnboundedInteger {
+    private LinkedList<Integer> data;
+    public UnboundedInteger(String digits) {}
+
+    public String toString() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+        }
+        scanner.close();
+     }
+}
